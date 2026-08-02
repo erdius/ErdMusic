12e514feb93f0e600d7913171d164b5104dd9602
David Erdman <david.51.erdman@gmail.com>
2026-08-01 21:01:15 -0400

    Bump version to 1.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.erdman.erdmusic"
         minSdk = 28
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.2.3"
+        versionCode = 16
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
